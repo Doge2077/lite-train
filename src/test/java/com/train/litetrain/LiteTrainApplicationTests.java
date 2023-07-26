ea47bd17af3628726a5fdd41e0878bfbce14644b
Dog2077 <1002302241@qq.com>
2023-07-26 14:21:38 +0800

    Create lite-train

---
+++ src/test/java/com/train/litetrain/LiteTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.train.litetrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteTrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
