19f5233346a3007944e2090f3dfa2a3822d42c17
Dog2077 <1002302241@qq.com>
2023-07-29 14:25:57 +0800

    Add common LogAspect

--- member/src/main/java/com/train/member/MemberApplication.java
+++ member/src/main/java/com/train/member/MemberApplication.java
@@ -1,12 +1,13 @@
 package com.train.member;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
-import org.slf4j.Logger;
-
+@ComponentScan("com.train")
 @SpringBootApplication
 public class MemberApplication {
 
