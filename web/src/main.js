1b33d2284e994d2cc9a653d154a97b5558702b89
Dog2077 <1002302241@qq.com>
2023-08-21 02:03:12 +0800

    5.4 集成 and-design-vue 并进行全局配置

--- web/src/main.js
+++ web/src/main.js
@@ -2,5 +2,15 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css';
+import * as Icons from '@ant-design/icons-vue';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App);
+app.use(Antd).use(store).use(router).mount('#app');
+
+// 全局使用图标
+const icons = Icons;
+for (const i in icons) {
+    app.component(i, icons[i]);
+}
