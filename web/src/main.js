b8f73ce4896e6e6e40ec38150b999f73f422723f
Dog2077 <1002302241@qq.com>
2023-08-22 16:54:38 +0800

    5.10 axios 添加拦截器打印日志

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css';
 import * as Icons from '@ant-design/icons-vue';
+import axios from "axios";
 
 const app = createApp(App);
 app.use(Antd).use(store).use(router).mount('#app');
@@ -14,3 +15,25 @@ const icons = Icons;
 for (const i in icons) {
     app.component(i, icons[i]);
 }
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+    console.log('请求参数：', config);
+    const _token = store.state.member.token;
+    if (_token) {
+        config.headers.token = _token;
+        console.log("请求headers增加token:", _token);
+    }
+    return config;
+}, error => {
+    return Promise.reject(error);
+});
+axios.interceptors.response.use(function (response) {
+    console.log('返回结果：', response);
+    return response;
+}, error => {
+    console.log('返回错误：', error);
+    return Promise.reject(error);
+});
--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -55,13 +55,13 @@ export default defineComponent({
     });
 
     const sendCode = () => {
-      axios.post("/member/member/send-code", {
+      axios.post("http://localhost:8000/member/member/send-code", {
         mobile: loginForm.mobile
       }).then(response => {
         let data = response.data;
         if (data.success) {
           notification.success({ description: '发送验证码成功！' });
-          loginForm.code = "8888";
+          loginForm.code = "1145";
         } else {
           notification.error({ description: data.message });
         }
@@ -69,7 +69,7 @@ export default defineComponent({
     };
 
     const login = () => {
-      axios.post("/member/member/login", loginForm).then((response) => {
+      axios.post("http://localhost:8000/member/member/login", loginForm).then((response) => {
         let data = response.data;
         if (data.success) {
           notification.success({ description: '登录成功！' });
