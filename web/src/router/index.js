176d22e38fba75cc8c72583ff0b03bca2e831814
Dog2077 <1002302241@qq.com>
2023-08-22 18:17:16 +0800

    5.12 添加后台管理界面，以及登录路由跳转

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,21 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  },
-  {
     path: '/login',
-    name: 'login',
     component: () => import('../views/login.vue')
+  },
+  {
+    path: '/',
+    component: () => import('../views/main.vue')
   }
 ]
 
