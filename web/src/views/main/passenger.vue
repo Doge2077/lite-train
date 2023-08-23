58a9470fc5a8eca2fc3416cb928a39c02c60376c
Dog2077 <1002302241@qq.com>
2023-08-23 15:11:29 +0800

    7.7 添加分页选项模块，可以根据选择进行分页

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -3,20 +3,29 @@
     <a-space>
       <a-button type="primary" @click="handleQuery()">刷新</a-button>
       <a-button type="primary" @click="onAdd">新增</a-button>
+      <a-select v-model="selectSize" style="width: 100px;">
+        <a-select-option value="5" @click="handleSelectSize(5)">5 条/页</a-select-option>
+        <a-select-option value="10" @click="handleSelectSize(10)">10 条/页</a-select-option>
+        <a-select-option value="20" @click="handleSelectSize(20)">20 条/页</a-select-option>
+      </a-select>
     </a-space>
   </p>
-  <a-table :dataSource="passengers"
-           :columns="columns"
-           :pagination="pagination"
-           @change="handleTableChange"
-           :loading="loading">
+  <a-table
+      :dataSource="passengers"
+      :columns="columns"
+      :pagination="pagination"
+      @change="handleTableChange"
+      :loading="loading"
+  >
     <template #bodyCell="{ column, record }">
       <template v-if="column.dataIndex === 'operation'">
         <a-space>
           <a-popconfirm
               title="删除后不可恢复，确认删除?"
               @confirm="onDelete(record)"
-              ok-text="确认" cancel-text="取消">
+              ok-text="确认"
+              cancel-text="取消"
+          >
             <a style="color: red">删除</a>
           </a-popconfirm>
           <a @click="onEdit(record)">编辑</a>
@@ -25,15 +34,20 @@
       <template v-else-if="column.dataIndex === 'type'">
         <span v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code">
           <span v-if="item.code === record.type">
-            {{item.desc}}
+            {{ item.desc }}
           </span>
         </span>
       </template>
     </template>
   </a-table>
-  <a-modal v-model:visible="visible" title="乘车人" @ok="handleOk"
-           ok-text="确认" cancel-text="取消">
-    <a-form :model="passenger" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
+  <a-modal
+      v-model:visible="visible"
+      title="乘车人"
+      @ok="handleOk"
+      ok-text="确认"
+      cancel-text="取消"
+  >
+    <a-form :model="passenger" :label-col="{ span: 4 }" :wrapper-col="{ span: 20 }">
       <a-form-item label="姓名">
         <a-input v-model:value="passenger.name" />
       </a-form-item>
@@ -42,8 +56,12 @@
       </a-form-item>
       <a-form-item label="旅客类型">
         <a-select v-model:value="passenger.type">
-          <a-select-option v-for="item in PASSENGER_TYPE_ARRAY" :key="item.code" :value="item.code">
-            {{item.desc}}
+          <a-select-option
+              v-for="item in PASSENGER_TYPE_ARRAY"
+              :key="item.code"
+              :value="item.code"
+          >
+            {{ item.desc }}
           </a-select-option>
         </a-select>
       </a-form-item>
@@ -53,11 +71,11 @@
 
 <script>
 import { defineComponent, ref, onMounted } from 'vue';
-import {notification} from "ant-design-vue";
-import axios from "axios";
+import { notification } from 'ant-design-vue';
+import axios from 'axios';
 
 export default defineComponent({
-  name: "passenger-view",
+  name: 'passenger-view',
   setup() {
     const PASSENGER_TYPE_ARRAY = window.PASSENGER_TYPE_ARRAY;
     const visible = ref(false);
@@ -72,32 +90,41 @@ export default defineComponent({
     });
     const passengers = ref([]);
     // 分页的三个属性名是固定的
+    let selectSize = ref(10);
     const pagination = ref({
       total: 0,
       current: 1,
-      pageSize: 10,
+      pageSize: selectSize.value,
     });
+    const handleSelectSize = (size) => {
+      selectSize.value = size;
+      pagination.value.pageSize = size;
+      handleQuery({
+        page: 1,
+        size: size,
+      });
+    };
     let loading = ref(false);
     const columns = [
-    {
-      title: '姓名',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: '身份证',
-      dataIndex: 'idCard',
-      key: 'idCard',
-    },
-    {
-      title: '旅客类型',
-      dataIndex: 'type',
-      key: 'type',
-    },
-    {
-      title: '操作',
-      dataIndex: 'operation'
-    }
+      {
+        title: '姓名',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: '身份证',
+        dataIndex: 'idCard',
+        key: 'idCard',
+      },
+      {
+        title: '旅客类型',
+        dataIndex: 'type',
+        key: 'type',
+      },
+      {
+        title: '操作',
+        dataIndex: 'operation',
+      },
     ];
 
     const onAdd = () => {
@@ -111,32 +138,32 @@ export default defineComponent({
     };
 
     const onDelete = (record) => {
-      axios.delete("/member/passenger/delete/" + record.id).then((response) => {
+      axios.delete('/member/passenger/delete/' + record.id).then((response) => {
         const data = response.data;
         if (data.success) {
-          notification.success({description: "删除成功！"});
+          notification.success({ description: '删除成功！' });
           handleQuery({
             page: pagination.value.current,
             size: pagination.value.pageSize,
           });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
 
     const handleOk = () => {
-      axios.post("/member/passenger/save", passenger.value).then((response) => {
+      axios.post('/member/passenger/save', passenger.value).then((response) => {
         let data = response.data;
         if (data.success) {
-          notification.success({description: "保存成功！"});
+          notification.success({ description: '保存成功！' });
           visible.value = false;
           handleQuery({
             page: pagination.value.current,
-            size: pagination.value.pageSize
+            size: pagination.value.pageSize,
           });
         } else {
-          notification.error({description: data.message});
+          notification.error({ description: data.message });
         }
       });
     };
@@ -145,41 +172,42 @@ export default defineComponent({
       if (!param) {
         param = {
           page: 1,
-          size: pagination.value.pageSize
+          size: pagination.value.pageSize,
         };
       }
       loading.value = true;
-      axios.get("/member/passenger/query-list", {
-        params: {
-          page: param.page,
-          size: param.size
-        }
-      }).then((response) => {
-        loading.value = false;
-        let data = response.data;
-        if (data.success) {
-          passengers.value = data.content.list;
-          // 设置分页控件的值
-          pagination.value.current = param.page;
-          pagination.value.total = data.content.total;
-        } else {
-          notification.error({description: data.message});
-        }
-      });
+      axios
+          .get('/member/passenger/query-list', {
+            params: {
+              page: param.page,
+              size: param.size,
+            },
+          })
+          .then((response) => {
+            loading.value = false;
+            let data = response.data;
+            if (data.success) {
+              passengers.value = data.content.list;
+              // 设置分页控件的值
+              pagination.value.current = param.page;
+              pagination.value.total = data.content.total;
+            } else {
+              notification.error({ description: data.message });
+            }
+          });
     };
 
     const handleTableChange = (pagination) => {
-      // console.log("看看自带的分页参数都有啥：" + pagination);
       handleQuery({
         page: pagination.current,
-        size: pagination.pageSize
+        size: pagination.pageSize,
       });
     };
 
     onMounted(() => {
       handleQuery({
         page: 1,
-        size: pagination.value.pageSize
+        size: pagination.value.pageSize,
       });
     });
 
@@ -188,15 +216,17 @@ export default defineComponent({
       passenger,
       visible,
       passengers,
+      selectSize,
       pagination,
       columns,
       handleTableChange,
       handleQuery,
+      handleSelectSize,
       loading,
       onAdd,
       handleOk,
       onEdit,
-      onDelete
+      onDelete,
     };
   },
 });
