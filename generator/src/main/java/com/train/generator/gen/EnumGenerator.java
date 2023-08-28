cc9f45497001ba0676254919081122c1af8bdb4d
Dog2077 <1002302241@qq.com>
2023-08-28 10:07:24 +0800

    8.3 优化了 pom 导入

--- generator/src/main/java/com/train/generator/gen/EnumGenerator.java
+++ generator/src/main/java/com/train/generator/gen/EnumGenerator.java
@@ -23,10 +23,10 @@ public class EnumGenerator {
         long begin = System.currentTimeMillis();
         try {
             toJson(PassengerTypeEnum.class, bufferObject, bufferArray);
-            toJson(TrainTypeEnum.class, bufferObject, bufferArray);
-            toJson(SeatTypeEnum.class, bufferObject, bufferArray);
-            toJson(SeatColEnum.class, bufferObject, bufferArray);
-            toJson(ConfirmOrderStatusEnum.class, bufferObject, bufferArray);
+//            toJson(TrainTypeEnum.class, bufferObject, bufferArray);
+//            toJson(SeatTypeEnum.class, bufferObject, bufferArray);
+//            toJson(SeatColEnum.class, bufferObject, bufferArray);
+//            toJson(ConfirmOrderStatusEnum.class, bufferObject, bufferArray);
 
             StringBuffer buffer = bufferObject.append("\r\n").append(bufferArray);
             writeJs(buffer);
