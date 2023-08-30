3c588f1d5920fb812bed578ddfbbd1ea7915a695
Dog2077 <1002302241@qq.com>
2023-08-30 17:00:50 +0800

    10.2 为 batch 模块添加持久层代码

---
+++ batch/src/main/java/com/train/batch/mapper/MemberMapper.java
@@ -0,0 +1,30 @@
+package com.train.batch.mapper;
+
+import com.train.batch.domain.Member;
+import com.train.batch.domain.MemberExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface MemberMapper {
+    long countByExample(MemberExample example);
+
+    int deleteByExample(MemberExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Member record);
+
+    int insertSelective(Member record);
+
+    List<Member> selectByExample(MemberExample example);
+
+    Member selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByExample(@Param("record") Member record, @Param("example") MemberExample example);
+
+    int updateByPrimaryKeySelective(Member record);
+
+    int updateByPrimaryKey(Member record);
+}
