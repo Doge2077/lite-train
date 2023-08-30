ab4725eb06f8eaba317a7d9a57600ec560631cf3
Dog2077 <1002302241@qq.com>
2023-08-30 14:51:11 +0800

    10.1 添加 batch 模块

---
+++ batch/src/main/java/com/train/batch/config/BatchApplication.java
@@ -0,0 +1,24 @@
+package com.train.batch.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@ComponentScan("com.train")
+@MapperScan("com.train.*.mapper")
+public class BatchApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(BatchApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(BatchApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Batch 启动成功！！");
+//        LOG.info("测试地址: \thttp://127.0.0.1:{}{}/hello", env.getProperty("server.port"), env.getProperty("server.servlet.context-path"));
+    }
+}
