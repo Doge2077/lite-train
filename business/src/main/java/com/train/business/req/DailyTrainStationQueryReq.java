c705feefebfa568b7cc93a848ae9ed888e70466d
Dog2077 <1002302241@qq.com>
2023-09-04 00:38:57 +0800

    11.3 添加每日车站功能

---
+++ business/src/main/java/com/train/business/req/DailyTrainStationQueryReq.java
@@ -0,0 +1,12 @@
+package com.train.business.req;
+
+import com.train.common.req.PageReq;
+
+public class DailyTrainStationQueryReq extends PageReq {
+
+    @Override
+    public String toString() {
+        return "DailyTrainStationQueryReq{" +
+                "} " + super.toString();
+    }
+}
---
+++ business/src/main/java/com/train/business/service/DailyTrainStationService.java
@@ -0,0 +1,71 @@
+package com.train.business.service;
+
+import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.date.DateTime;
+import cn.hutool.core.util.ObjectUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.train.common.resp.PageResp;
+import com.train.common.util.SnowUtil;
+import com.train.business.domain.DailyTrainStation;
+import com.train.business.domain.DailyTrainStationExample;
+import com.train.business.mapper.DailyTrainStationMapper;
+import com.train.business.req.DailyTrainStationQueryReq;
+import com.train.business.req.DailyTrainStationSaveReq;
+import com.train.business.resp.DailyTrainStationQueryResp;
+import jakarta.annotation.Resource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DailyTrainStationService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DailyTrainStationService.class);
+
+    @Resource
+    private DailyTrainStationMapper dailyTrainStationMapper;
+
+    public void save(DailyTrainStationSaveReq req) {
+        DateTime nowTime = DateTime.now();
+        DailyTrainStation dailyTrainStation = BeanUtil.copyProperties(req, DailyTrainStation.class);
+        if (ObjectUtil.isNull(dailyTrainStation.getId())) {
+            dailyTrainStation.setId(SnowUtil.getSnowflakeNextId());
+            dailyTrainStation.setCreateTime(nowTime);
+            dailyTrainStation.setUpdateTime(nowTime);
+            dailyTrainStationMapper.insert(dailyTrainStation);
+        } else {
+            dailyTrainStation.setCreateTime(dailyTrainStation.getCreateTime());
+            dailyTrainStation.setUpdateTime(nowTime);
+            dailyTrainStationMapper.updateByPrimaryKey(dailyTrainStation);
+        }
+    }
+
+    public PageResp<DailyTrainStationQueryResp> queryList(DailyTrainStationQueryReq req) {
+        DailyTrainStationExample dailyTrainStationExample = new DailyTrainStationExample();
+        dailyTrainStationExample.setOrderByClause("id desc");
+        DailyTrainStationExample.Criteria criteria = dailyTrainStationExample.createCriteria();
+
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<DailyTrainStation> dailyTrainStationList = dailyTrainStationMapper.selectByExample(dailyTrainStationExample);
+
+        PageInfo<DailyTrainStation> pageInfo = new PageInfo<>(dailyTrainStationList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        List<DailyTrainStationQueryResp> list = BeanUtil.copyToList(dailyTrainStationList, DailyTrainStationQueryResp.class);
+
+        PageResp<DailyTrainStationQueryResp> pageResp = new PageResp<>();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public void delete(Long id) {
+        dailyTrainStationMapper.deleteByPrimaryKey(id);
+    }
+}
