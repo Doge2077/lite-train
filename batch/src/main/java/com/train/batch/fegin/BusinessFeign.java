3b462abcd9398197c4224c5a882bee4bae4edb55
Dog2077 <1002302241@qq.com>
2023-09-05 00:02:46 +0800

    11.8 完善跑批管理功能

--- batch/src/main/java/com/train/batch/fegin/BusinessFeign.java
+++ batch/src/main/java/com/train/batch/fegin/BusinessFeign.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Date;
 
-@FeignClient(value = "business", fallback = BusinessFeignFallback.class)
-// @FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
+//@FeignClient(value = "business")
+ @FeignClient(name = "business", url = "http://127.0.0.1:8002/business")
 public interface BusinessFeign {
 
-    @GetMapping("/business/admin/daily-train/gen-daily/{date}")
+    @GetMapping("/admin/daily-train/gen-daily/{date}")
     CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") Date date);
 }
