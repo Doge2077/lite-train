e7f5ca22823ecdaca8671e03e49ad62eb45bb228
Dog2077 <1002302241@qq.com>
2023-09-06 23:54:51 +0800

    12.3 添加会员端余票查询功能

---
+++ business/src/main/java/com/train/business/controller/DailyTrainTicketController.java
@@ -0,0 +1,27 @@
+package com.train.business.controller;
+
+import com.train.business.req.DailyTrainTicketQueryReq;
+import com.train.business.resp.DailyTrainTicketQueryResp;
+import com.train.business.service.DailyTrainTicketService;
+import com.train.common.resp.CommonResp;
+import com.train.common.resp.PageResp;
+import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/daily-train-ticket")
+public class DailyTrainTicketController {
+
+    @Resource
+    private DailyTrainTicketService dailyTrainTicketService;
+
+    @GetMapping("/query-list")
+    public CommonResp<PageResp<DailyTrainTicketQueryResp>> queryList(@Valid DailyTrainTicketQueryReq req) {
+        PageResp<DailyTrainTicketQueryResp> list = dailyTrainTicketService.queryList(req);
+        return new CommonResp<>(list);
+    }
+
+}
---
+++ business/src/main/java/com/train/business/controller/TrainController.java
@@ -0,0 +1,27 @@
+package com.train.business.controller;
+
+
+import com.train.business.resp.TrainQueryResp;
+import com.train.business.service.TrainService;
+import com.train.common.resp.CommonResp;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/train")
+public class TrainController {
+
+    @Resource
+    private TrainService trainService;
+
+    @GetMapping("/query-all")
+    public CommonResp<List<TrainQueryResp>> queryList() {
+        List<TrainQueryResp> list = trainService.queryAll();
+        return new CommonResp<>(list);
+    }
+
+}
